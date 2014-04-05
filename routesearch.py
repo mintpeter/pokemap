fa49b683f6391846028ee004a0b88571285c3197
Zack Marvel <zpmarvel@gmail.com>
2014-04-05 18:48:21 -0400

    Finalize the script?
    I think the script is ready to go. I guess it's time to start
    generating some image maps from it.

--- routesearch.py
+++ routesearch.py
@@ -12,8 +12,9 @@ class Patch:
     def add_grass(self):
         self.x2 += SPRITE_SIZE
 
-class Route(Image.Image):
+class Route:
     def __init__(self, route_path, grass_path):
+        self.route_path = route_path
         self.route = Image.open(route_path)
         self.width, self.height = self.route.size
 
@@ -21,6 +22,8 @@ class Route(Image.Image):
         
         self.xstart, self.ystart = self.find_grass_start()
         self.xoffset = self.xstart % SPRITE_SIZE
+
+        self.grass_patches = self.find_grass_patches
 
     def find_grass_start(self):
         for y1 in range(0, self.height - SPRITE_SIZE + 1):
@@ -34,7 +37,7 @@ class Route(Image.Image):
                     return x1, y1
 
     def find_grass_patches(self):
-        self.grass_patches = []
+        grass_patches = []
         x1, y1 = self.xstart, self.ystart
 
         while y1 <= self.height - SPRITE_SIZE:
@@ -47,16 +50,16 @@ class Route(Image.Image):
                 diff = ImageChops.difference(square, self.grass)
 
                 if sum(ImageMath.eval('int(diff)', diff=diff).histogram()) == 0:
-                    if self.grass_patches and\
-                        self.grass_patches[-1].y1 == y1 and\
-                        self.grass_patches[-1].x2 == x1:
-                        self.grass_patches[-1].add_grass()
+                    if grass_patches and\
+                        grass_patches[-1].y1 == y1 and\
+                        grass_patches[-1].x2 == x1:
+                        grass_patches[-1].add_grass()
                     else:
-                        self.grass_patches.append(Patch(x1, x2, y1))
+                        grass_patches.append(Patch(x1, x2, y1))
                 x1 += SPRITE_SIZE
             y1 += SPRITE_SIZE
 
-        return self.grass_patches
+        return grass_patches
 
 def main(route_path, grass_path):
     route = Route(route_path, grass_path)
@@ -70,7 +73,5 @@ if __name__ == '__main__':
     patches = main(route_path, grass_path)
     total = 0
     for patch in patches:
-        print(patch.x1, patch.y1, patch.x2, patch.y2)
+        print("{},{},{},{}".format(patch.x1, patch.y1, patch.x2, patch.y2))
         total += 1
-    print('{} rows of grass'.format(total))
-
