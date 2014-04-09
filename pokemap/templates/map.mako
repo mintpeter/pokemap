b8ee41f372b3484929bb62fb0f725772008a73e8
Zack Marvel <zpmarvel@gmail.com>
2014-04-08 18:53:30 -0900

    Get moving on pokedex queries
    There's a lot of work still to do, but it "works". Obviously, the format leaves
    a lot to be desired, but the actual queries are working well enough, and the
    necessary data is being displayed. Javascript probably needs a little work.
    
    TO DO:
    -   Make the model more flexible. More specifically, I want to change the route
        to /map/{region}/{route}. Need some way to specify what generation we're
        talking about, too. /map/{region}/{generation_id}/{route} doesn't work,
        though, because the generation_id is too ambiguous for a URL.
        /map/{region}/gen{generation_id}/{route} would be okay.
    -   Obviously, formatting. Somehow I need this to work for vertical and
        horizontal routes. I guess the table(s) will have to be displayed below
        horizontal ones and beside vertical ones. And make it look decent.
    -   Might need to rethink my db schema. It would probably be wise to just join
        my table(s) to the Pokedex's locations table...
    -   After I get my database lined out, I'll definitely need to gather some more
        data. On a brighter note, I think my script can correctly detect water in
        addition to tall grass. Going to stick to FRLG for the moment, then move on
        to RSE, then backwards, then forwards.
    
    Hopefully all my homework will go quickly tomorrow so I can hammer out some more
    code.

--- pokemap/templates/map.mako
+++ pokemap/templates/map.mako
@@ -1,15 +1,52 @@
 <%inherit file="base.mako"/>
 
-<img src="/static/route/${route_id}.png" alt="Route ${route_id}" usemap="#route${route_id}"/>
+<img src="/static/route/1.png" alt="Route 1" usemap="#route1"/>
 
-<map name="route${route_id}">
+<map name="route1">
 %for patch in patches:
-    <area shape="rect" coords="${patch.x1},${patch.y1},${patch.x2},${patch.y2}" class="grass-patch" alt="grass" href="#"/>
+    <area shape="rect" coords="${patch.x1},${patch.y1},${patch.x2},${patch.y2}"
+        class="grass-patch" alt="grass" href="#"/>
 %endfor
 </map>
 
 <span class="grass-patch">
-Some talking about the grass stuff on route ${route_id}
+Some talking about the grass stuff on da route.
+
+Encounter Pokemon:
+##%for encounter in encounters:
+##<p>
+##    ${encounter.pokemon.name} &mdash; ${encounter.min_level} &ndash; ${encounter.max_level}<br/>
+##    %for condition_value in encounter.condition_values:
+##        ${condition_value.name}, default: ${condition_value.is_default}<br/>
+##    %endfor
+##    ${encounter.slot.rarity}<br/>
+##</p>
+##%endfor
+
+%for method in sorted_encounters:
+<table class="encounters">
+    <thead>
+        <tr>
+            <th>Pokemon</th>
+    %for version in versions:
+            <th>${version.name}</th>
+    %endfor
+        </tr>
+    </thead>
+    <tbody>
+    %for pokemon in sorted_encounters[method]:
+        <tr>
+            <td>${pokemon.name}</td>
+        %for version in sorted_encounters[method][pokemon]:
+            <td>${sorted_encounters[method][pokemon][version]}%</td>
+        %endfor
+        </tr>
+    %endfor
+    </tbody>
+</table>
+%endfor
+            
+
 </span>
 <span class="water-patch">
 This one talks about water. Water is great.
--- pokemap/views.py
+++ pokemap/views.py
@@ -1,12 +1,20 @@
+from collections import defaultdict
+
 from pyramid.response import Response
 from pyramid.view import view_config, notfound_view_config
 
+from sqlalchemy import and_
 from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
     Patch
     )
+
+from pokedex.db import connect
+import pokedex.db.tables as t
+
+PDSession = connect('sqlite:////home/zack/playground/pokemap/pokedex/pokedex/data/pokedex.sqlite')
 
 
 @view_config(route_name='home', renderer='home.mako')
@@ -15,11 +23,54 @@ def my_view(request):
 
 @view_config(route_name='map', renderer='map.mako')
 def view_map(request):
-    route_id = request.matchdict['rid']
+    location_identifier = request.matchdict['route_identifier']
 
-    patches = DBSession.query(Patch).filter(Patch.routeid == route_id).all()
+    location_area = PDSession.query(t.LocationArea).join(t.Location)\
+                    .filter(t.Location.identifier == location_identifier).first()
+#    encounters = PDSession.query(t.Encounter).join(t.Version)\
+#                    .filter(t.Version.version_group_id == 7)
 
-    return {'route_id': route_id, 'patches': patches}
+    encounters = PDSession.query(t.Encounter).join(t.Encounter.version)\
+                    .filter(t.Version.version_group_id == 7)\
+                    .join(t.Encounter.location_area)\
+                    .join(t.LocationArea.location)\
+                    .filter(t.Location.identifier == location_identifier)\
+#                    .join(t.Encounter.pokemon)\
+#                    .join(t.Encounter.slot)\
+#                    .join(t.EncounterSlot.method)
+
+    # Collapse redundant encounters
+
+
+    # Structure the pokemon that are going to be listed.
+    # First, the method of encounter.
+    # Second, the game version
+    # Third, the pokemon.
+    # Finally, the frequency of the pokemon's occurrence.
+    # Based on the original:
+    # https://github.com/veekun/spline-pokedex/blob/master/splinext/pokedex/controllers/pokedex.py
+    sorted_encounters = defaultdict(
+                            lambda: defaultdict(
+                                lambda: defaultdict(lambda: 0)
+                            )
+                        )
+
+    versions = []
+
+    for encounter in encounters:
+        sorted_encounters\
+            [encounter.slot.method]\
+            [encounter.pokemon]\
+            [encounter.version]\
+            += encounter.slot.rarity
+        if encounter.version not in versions:
+            versions.append(encounter.version)
+
+    patches = DBSession.query(Patch).filter(Patch.routeid == 1).all()
+
+    return {'patches': patches,
+            'versions': versions,
+            'sorted_encounters': sorted_encounters}
 
 @notfound_view_config(renderer='404.mako', append_slash=True)
 def not_found(request):
