04026d5ba7c4127d40e971379c5b69b11b9901aa
Zack Marvel <zpmarvel@gmail.com>
2014-04-11 07:10:01 -0900

    Add patch_types table
    Seems useful. Primary problem? When hovering over water, I want to see surf
    encounters and fishing encounters. Since these have different encounter methods,
    I need to group the methods together.

--- pokemap/models.py
+++ pokemap/models.py
@@ -1,6 +1,11 @@
+import os
+
 from sqlalchemy import (
+    create_engine,
     Column,
+    ForeignKey,
     Integer,
+    Unicode
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,30 +13,55 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
+    backref
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+import pokedex.db.tables as t
+
 Base = declarative_base()
 
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 
 class Patch(Base):
     __tablename__ = 'patches'
     id = Column(Integer, primary_key=True)
-    genid = Column(Integer)
-    routeid = Column(Integer)
-    typeid = Column(Integer)
+    generation_id = Column(Integer)
+    location_id = Column(Integer)
+    patch_type_id = Column(Integer, ForeignKey('patch_types.id'))
     x1 = Column(Integer)
     y1 = Column(Integer)
     x2 = Column(Integer)
     y2 = Column(Integer)
 
-    def __init__(self, genid, routeid, typeid, x1, y1, x2, y2):
-        self.genid = genid
-        self.routeid = routeid
-        self.typeid = typeid
+    def __init__(self,\
+                    generation_id,\
+                    location_id,\
+                    patch_type_id,\
+                    x1,\
+                    y1,\
+                    x2,\
+                    y2):
+        self.generation_id = generation_id
+        self.location_id = location_id
+        self.patch_type_id = patch_type_id
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+
+class PatchType(Base):
+    __tablename__ = 'patch_types'
+    id = Column(Integer, primary_key=True)
+    encounter_method_id = Column(Integer)
+    name = Column(Unicode(32))
+
+    def __init__(self,\
+                    encounter_method_id,\
+                    name):
+        self.encounter_method_id = encounter_method_id
+        self.name = name
+
+Patch.patch_type = relationship(PatchType, lazy='joined')
